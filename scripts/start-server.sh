7ce883e68b93bf17a181e16241aaac09863e008b
Petko D. Petkov <pdp@websecurify.com>
2018-02-19 12:12:51 +0000

    More agile architecture.

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const { Transform } = require('stream')
+
 const { Proxy } = require('../lib/proxy')
 const { CertManager } = require('../lib/cert-manager')
 const { generateRootKeysAndCert } = require('../lib/cert-utils')
@@ -9,5 +11,29 @@ const defaultRootKeysAndCert = generateRootKeysAndCert()
 const certManager = new CertManager({defaultRootKeysAndCert})
 
 const proxy = new Proxy({certManager})
+
+proxy.on('request', (req) => {
+    req.pipeline.first(new Transform({
+        objectMode: true,
+
+        transform(chunk, encoding, callback) {
+            console.log('>>>', chunk.toString())
+
+            callback(null, chunk)
+        }
+    }))
+})
+
+proxy.on('response', (res) => {
+    res.pipeline.first(new Transform({
+        objectMode: true,
+
+        transform(chunk, encoding, callback) {
+            console.log('<<<', chunk.toString())
+
+            callback(null, chunk)
+        }
+    }))
+})
 
 proxy.listen(8080)
